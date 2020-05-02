9d2efd8c39941d403bca1f901144144c9ed04b61
Joel Coelho <joelcoelho@iMac-de-Joel.local>
2020-05-01 21:14:16 -0300

    Styling search

--- lib/screens/countries.dart
+++ lib/screens/countries.dart
@@ -1,14 +1,11 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Countries extends StatefulWidget {
   var argument;
 
-  Countries({
-    @required this.argument
-  });
+  Countries({@required this.argument});
 
   @override
   State<StatefulWidget> createState() {
@@ -19,6 +16,7 @@ class Countries extends StatefulWidget {
 class _CountriesState extends State<Countries> {
   var countryList = List();
   var filteredCountryList = List();
+  double _width = 56;
 
   searchCountryData(countryName, countryList) {
     print('$countryName from countries.dart');
@@ -37,8 +35,8 @@ class _CountriesState extends State<Countries> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromRGBO(41, 41, 41, 1),
-        appBar: AppBar(
+      backgroundColor: Color.fromRGBO(41, 41, 41, 1),
+      appBar: AppBar(
         backgroundColor: Color.fromRGBO(38, 38, 38, 1),
         title: Text(
           'Locations',
@@ -49,51 +47,80 @@ class _CountriesState extends State<Countries> {
         ),
         centerTitle: true,
       ),
-      body:
-      Column(
+      body: Column(
         children: <Widget>[
-          Container(
-            color: Colors.white,
-            child:  TextField(
-                onChanged: (string) {
-                  setState(() {
-                    filteredCountryList = countryList
-                        .where((u) => (u
-                        .toLowerCase()
-                        .startsWith(string.toLowerCase())))
-                        .toList();
-                  });
-                },
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(10.0),
-                    hintText: 'Search by country',
-                    fillColor: Color(0xFFFFF494)
-                ),
-              ),
-          ),
           Expanded(
-            child:  ListView.builder(
-              primary: true,
-              itemCount: filteredCountryList.length,
-              itemBuilder: (context, index) {
-                return Material(
-                  child: ListTile(
-                      title: Center(
-                          child: Text(
-                            filteredCountryList[index],
-                            style: GoogleFonts.montserrat(
-                                fontSize: 18,
-                                color: Color(0xFFE1E1E1),
-                                fontWeight: FontWeight.w300),
-                          )),
-                      onTap: () {
-                        searchCountryData(filteredCountryList[index], countryList);
-                      }),
-                  color: Color.fromRGBO(41, 41, 41, 1),
-                );
-              }),
-          )],
-      )
+            child: ListView.builder(
+                primary: true,
+                itemCount: filteredCountryList.length,
+                itemBuilder: (context, index) {
+                  return Material(
+                    child: ListTile(
+                        title: Center(
+                            child: Text(
+                          filteredCountryList[index],
+                          style: GoogleFonts.montserrat(
+                              fontSize: 18,
+                              color: Color(0xFFE1E1E1),
+                              fontWeight: FontWeight.w300),
+                        )),
+                        onTap: () {
+                          searchCountryData(
+                              filteredCountryList[index], countryList);
+                        }),
+                    color: Color.fromRGBO(41, 41, 41, 1),
+                  );
+                }),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Visibility(
+                child: AnimatedContainer(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    color: Colors.white12,
+                  ),
+                  duration: Duration(milliseconds: 400),
+                  width: _width,
+                  height: 56,
+                  child: TextField(
+                    onChanged: (string) {
+                      setState(() {
+                        filteredCountryList = countryList
+                            .where((u) =>
+                        (u.toLowerCase().startsWith(string.toLowerCase())))
+                            .toList();
+                      });
+                    },
+                    textAlignVertical: TextAlignVertical.center,
+                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 40.0, vertical: 25.0),
+                        border: InputBorder.none),
+                  ),
+                ),
+                visible: _width == 56 ? false : true,
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _width = MediaQuery. of(context).size.width;
+          });
+        },
+        elevation: 0,
+        child: Icon(
+          Icons.search,
+          color: Colors.black87,
+        ),
+        backgroundColor: Color(0xFFFFF494),
+      ),
     );
   }
 }
